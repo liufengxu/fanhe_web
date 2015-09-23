d4e3efee2de6da40d5576044d01f704ac4b87716
liufengxu <onx0320@126.com>
2015-09-23 20:58:50 +0800

    easiest version

--- test.py
+++ test.py
@@ -9,7 +9,7 @@
 description:
 author: liufengxu
 date: 2015-09-22 16:54:50
-last modified: 2015-09-23 15:12:02
+last modified: 2015-09-23 20:32:13
 version:
 """
 
@@ -25,6 +25,17 @@ app = Flask(__name__)
 TOKEN = "biubiz"
 
 
+def load_index(index_file):
+    index_dict = {}
+    with open(index_file) as fp:
+        for line in fp:
+            segs = line[:-1].split('\t')
+            key = segs[0].decode('utf8')
+            value = ','.join(segs[1:]).decode('utf8')
+            index_dict[key] = value
+    return index_dict
+
+
 def responseMsg():
     postStr = request.data
     logging.debug('%s', postStr)
@@ -34,6 +45,7 @@ def responseMsg():
     toUsername = root.find('ToUserName').text
     keyword = root.find('Content').text.strip()
     cur_time = str(int(time.time()))
+    # 返回消息模板
     textTpl = ("<xml>"
                "<ToUserName><![CDATA[%s]]></ToUserName>"
                "<FromUserName><![CDATA[%s]]></FromUserName>"
@@ -43,11 +55,21 @@ def responseMsg():
                "<FuncFlag>0</FuncFlag>"
                "</xml>")
     logging.debug('%s', textTpl)
+    # 初始化索引
+    mem_index = load_index('./index.txt')
     if keyword:
         msgType = "text"
         contentStr = keyword
+        logging.debug('%s', contentStr)
+        # 从微信服务器接收到的数据是Unicode的
+        if contentStr == '你好'.decode('utf8'):
+            contentStr = '你好，我是小饭盒'
+        elif contentStr in mem_index:
+            contentStr = mem_index[contentStr]
+        else:
+            contentStr = '我不知道你在说神马'
         ret_xml = textTpl % (fromUsername, toUsername, cur_time,
-                          msgType, contentStr)
+                             msgType, contentStr)
         logging.debug('%s', ret_xml)
         return ret_xml
     else:
@@ -80,6 +102,7 @@ def test():
     nonce = str(request.args.get('nonce', ''))
     if checkSignature(signature, timestamp, nonce):
         return responseMsg()
+        # 接口信息配置验证时使用这个return
         return '%s' % echostr
     else:
         return 'oops: %s' % echostr
